1182fae757bc70e8fb9c74ff08600e4ba21ac7dc
Juan Tellez <juan.tellez01@utrgv.edu>
2018-05-09 15:27:09 -0500

    GradientGen alpha

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-var h3 = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var logo = document.getElementById("logo");
+var h3 = document.getElementById("background");
 
-h3.textContent =body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+logo.style.background = "linear-gradient(45deg, " + color1.value + ", " + color2.value + ")";
+h3.textContent = body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
+	body.style.background =
+	"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
 	+ ")";
-	
+
 	h3.textContent =body.style.background+";";
+	logo.style.background = "linear-gradient(45deg, " + color1.value + ", " + color2.value + ")";
 }
 
 color1.addEventListener("input", setGradient);
